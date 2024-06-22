8269a76c2580b9ad11d0fd1464241c24d9e47a51
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-22 17:10:42 -0500

    Start buttonHandler logic to determine where to send event data based on button class.

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,23 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const calcButtonGroup = document.querySelector('.calc-body');
+const numberDisplay = document.querySelector('.num-display p');
+
+
+calcButtonGroup.addEventListener('click', buttonHandler);
+
+function buttonHandler(event) {
+    let targetTargetClassList = event.target.classList;
+    let element = event.target;
+    let string = element.textContent;
+    console.table(string);
+}
+
+function displayKey(element, string) {
+
+}
+
 function operate(a, b, operator) {
     return add(a, b);
 }
@@ -21,8 +38,3 @@ function multply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
-console.log("Add 4 + 5: " + add(4, 5));
-console.log("Subtract 14 - 5: " + subtract(14, 5));
-console.log("Mulitply 4 x 5: " + multply(4, 5));
-console.log("Divide 50 / 5: " + divide(50, 5));
