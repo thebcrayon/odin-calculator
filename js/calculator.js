e7406f6f82fe438d806eff3d8d9906a84cb1b768
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-25 00:05:46 -0500

    Branch and refactor the initial setup
    I was able to flowchart the logic of the
    calculator and made revisions. Major approach
    includes storing variable inside calcExp obj.
    
    The old version of showing numbers on the screen
    was rewritten to updateDisplay to handle clearing
    and displaying task. This function takes two
    arugments:
    
    • String - Text to display on screen.
    • True / False - Flag that determines if clearing
      screen is part of the updateDisplay from
      where it was called.

--- js/calculator.js
+++ js/calculator.js
@@ -1,34 +1,88 @@
-let firstNumber;
-let secondNumber;
-let operator;
-
 const calcButtonGroup = document.querySelector('.calc-body');
-const numberDisplay = document.querySelector('.num-display p');
-
-
+const calcDisplay = document.querySelector('.num-display p');
 calcButtonGroup.addEventListener('click', buttonHandler);
 
+const calcExp = {
+    init: true,
+    operandA: undefined,
+    operandB: undefined,
+    operator: undefined,
+    currentButton: undefined,
+    clear: function () {
+        this.init = true;
+        this.operandA = undefined;
+        this.operandB = undefined;
+        this.operator = undefined;
+        this.currentButton = undefined;
+    },
+}
+
 function buttonHandler(event) {
-    let buttonInfo = {
+    calcExp['currentButton'] = {
         classList: Array.from(event.target.classList),
         element: event.target,
-        text: event.target.textContent,
+        textValue: event.target.textContent,
     }
-    console.log(buttonInfo.classList);
-    clearDisplay(buttonInfo.classList);
-    addToDisplay(buttonInfo.classList, buttonInfo.text);
+
+    let buttonClassList = calcExp.currentButton.classList;
+    const BUTTON_AC = buttonClassList.includes('clear');
+    const BUTTON_NUM = buttonClassList.includes('number');
+    const BUTTON_MOD = (buttonClassList.includes('pos-neg') || buttonClassList.includes('percent'));
+
+    if (BUTTON_AC) {
+        resetCalculator();
+    }
+
+    if (BUTTON_MOD) {
+        let OPERAND_NO_A_NO_B = (!calcExp.operandA) && (!calcExp.operandB);
+        let OPERAND_A_NO_B = (calcExp.operandA) && (!calcExp.operandB);
+        let OPERAND_A_B = (calcExp.operandA) && (calcExp.operandB);
+
+        if (calcDisplay.textContent !== '0') {
+            if (OPERAND_NO_A_NO_B) {
+                // 1. Modify and 2. Set operandA
+            } else if (OPERAND_A_NO_B || OPERAND_A_B) {
+                // 1. Modify and 2 set operand B
+            }
+        } else {
+            calcExp.init = true;
+        }
+    }
+
+    if (BUTTON_NUM) {
+        if (calcExp.init) {
+            updateDisplay(calcExp.currentButton.textValue, true);
+            calcExp.init = false;
+        } else if (calcExp.operandA) {
+            if (calcExp.operator) {
+                updateDisplay(calcExp.currentButton.textValue, true);
+            } else {
+                updateDisplay(calcExp.currentButton.textValue, true);
+                calcExp.operandA = undefined;
+            }
+        } else {
+            updateDisplay(calcExp.currentButton.textValue, false);
+        }
+    }
+
 }
 
-function clearDisplay(ary) {
-    if (ary.includes('clear')){
-        numberDisplay.textContent = '';
-    }
+function updateSign(string) {
+
 }
 
-function addToDisplay(ary, string) {
-    if (ary.includes('display')){
-        numberDisplay.append(string);
-    }
+function convertToPercent(string) {
+
+}
+
+function resetCalculator() {
+    calcExp.clear();
+    updateDisplay();
+}
+
+function updateDisplay(string = '0', clearDisplay = true) {
+    if (clearDisplay) { calcDisplay.textContent = ''; }
+    calcDisplay.append(string);
 }
 
 function operate(a, b, operator) {
