933aa99a40b3ea3db6045202dd5436fe0f98cb8c
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-07-01 13:02:33 -0500

    Refactor code to 'look back' logic
    Changed the calculator logic by storing previous
    button information to be able to 'look back' at
    the previous button to determine what logical
    steps we need to take with the current button.
    
    Equals operator doesn't yet work as expected.
    Need to do logic that disables decimal button when
    interacting with modifier buttons. Currently we
    don't get back decimal use after modifying number
    and/or pressing operator after modification.

--- js/calculator.js
+++ js/calculator.js
@@ -1,81 +1,149 @@
 const calcButtonGroup = document.querySelector('.calc-body');
 const calcDisplay = document.querySelector('.num-display p');
-calcButtonGroup.addEventListener('click', buttonHandler);
+const decimalBtn = document.querySelector('.decimal');
+const clearBtn = document.querySelector('.clear');
 
 const calcExp = {
-    init: true,
-    operandA: 0,
-    operandB: 0,
+    operandA: undefined,
     operator: undefined,
     currentButton: undefined,
+    previousButton: undefined,
     clear: function () {
-        this.init = true;
         this.operandA = undefined;
-        this.operandB = undefined;
         this.operator = undefined;
         this.currentButton = undefined;
+        this.previousButton = undefined;
     },
 }
 
+calcButtonGroup.addEventListener('click', buttonHandler);
+
 function buttonHandler(event) {
+    calcExp['previousButton'] = calcExp['currentButton'];
     calcExp['currentButton'] = {
         classList: Array.from(event.target.classList),
-        element: event.target,
         textValue: event.target.textContent,
     }
 
-    let buttonClassList = calcExp.currentButton.classList;
+    const buttonClassList = calcExp.currentButton.classList;
     const BUTTON_AC = buttonClassList.includes('clear');
     const BUTTON_NUM = buttonClassList.includes('number');
+    const BUTTON_DECIMAL = buttonClassList.includes('decimal');
     const BUTTON_OPERATOR = buttonClassList.includes('operator');
     const BUTTON_EQUALS = buttonClassList.includes('equals');
-    const BUTTON_MOD = (buttonClassList.includes('pos-neg') || buttonClassList.includes('percent'));
+    const BUTTON_MODIFIER = (buttonClassList.includes('pos-neg') || buttonClassList.includes('percent'));
+    const currentButtonValue = calcExp.currentButton.textValue;
 
     if (BUTTON_AC) {
         resetCalculator();
     }
 
     if (BUTTON_NUM) {
-        if (calcExp.init) {
-            updateDisplay(calcExp.currentButton.textValue, true);
-            calcExp.init = false;
-        } else if (calcExp.operandA) {
-            if (calcExp.operator) {
-                updateDisplay(calcExp.currentButton.textValue, false);
+        if (calcExp.previousButton) {
+            const previousButton = calcExp.previousButton.classList[0];
+            switch (previousButton) {
+                case 'number':
+                case 'decimal':
+                    updateDisplay(currentButtonValue, false);
+                    break;
+                case 'operator':
+                    updateDisplay(currentButtonValue, true);
+                    break;
             }
         } else {
-            updateDisplay(calcExp.currentButton.textValue, false);
+            updateDisplay(currentButtonValue, true);
         }
     }
 
     if (BUTTON_OPERATOR) {
-        calcExp.init = true;
-        if (BUTTON_EQUALS) {
-            if (calcExp.operandA) {
-                const a = parseFloat(calcExp.operandA);
-                const b = parseFloat(getDisplayValue());
-                updateDisplay(operate(a, b, calcExp.operator), true);
+        if (calcExp.previousButton) {
+            const previousButton = calcExp.previousButton.classList[0];
+            switch (previousButton) {
+                case 'clear':
+                    setOperandA();
+                    setOperator();
+                    break;
+                case 'number':
+                    if (calcExp.operandA) {
+                        //This order matters. Operator && lastbutton == number. 
+                        //setOperandA stores screen content to continue computing.
+                        sendToOperate();
+                        setOperandA();
+                        decimalBtn.disabled = false;
+                    } else {
+                        setOperandA();
+                        setOperator();
+                        decimalBtn.disabled = false;
+                    }
+                    break;
+                case 'operator':
+                    setOperator();
+                    break;
+                case 'modifier':
+                    setOperandA();
+                    setOperator();
+                    break;
             }
-        } else if (calcExp.operator) {
-            setOperator();
         } else {
-            setOperator();
             setOperandA();
+            setOperator();
         }
     }
 
+    if (BUTTON_DECIMAL) {
+        if (calcExp.previousButton) {
+            const previousButton = calcExp.previousButton.classList[0];
+            switch (previousButton) {
+                case 'number':
+                    if (countDecimals() == 0) {
+                        updateDisplay(currentButtonValue, false);
+                        decimalBtn.disabled = true;
+                    }
+                    break;
+                case 'operator':
+                    updateDisplay(`0${currentButtonValue}`, true);
+                    break;
+            }
+
+        } else {
+            updateDisplay(currentButtonValue, false);
+            decimalBtn.disabled = true;
+        }
+    }
+
+    if (BUTTON_MODIFIER) {
+        const currentDisplayValue = parseFloat(getDisplayValue());
+        if (currentDisplayValue !== 0) {
+            modifyNumber(currentDisplayValue, currentButtonValue);
+            setOperandA();
+        } else {
+            alert('trying to modify 0 value');
+        }
+    }
+}
+
+function sendToOperate() {
+    const a = calcExp.operandA;
+    const b = getDisplayValue();
+    updateDisplay(operate(a, b, calcExp.operator), true);
+}
+
+function countDecimals() {
+    return calcDisplay.textContent.split('')
+        .reduce((totalDecimals, item) => {
+            if (item == '.') {
+                totalDecimals += 1;
+            }
+            return totalDecimals;
+        }, 0);
 }
 
 function getDisplayValue() {
-    return calcDisplay.textContent;
+    return parseFloat(calcDisplay.textContent);
 }
 
 function setOperandA() {
-    calcExp.operandA = calcDisplay.textContent;
-}
-
-function setOperandB() {
-    calcExp.operandB = calcDisplay.textContent;
+    calcExp.operandA = parseFloat(calcDisplay.textContent);
 }
 
 function setOperator() {
@@ -84,12 +152,24 @@ function setOperator() {
 
 function resetCalculator() {
     calcExp.clear();
+    decimalBtn.disabled = false;
     updateDisplay();
 }
 
 function updateDisplay(string = '0', clearDisplay = true) {
     if (clearDisplay) { calcDisplay.textContent = ''; }
     calcDisplay.append(string);
+}
+
+function modifyNumber(number, modifier) {
+    switch (modifier) {
+        case '+/-':
+            updateDisplay(multply(number, -1), true);
+            break;
+        case '%':
+            updateDisplay(divide(number, 100), true);
+            break;
+    }
 }
 
 function operate(a, b, operator) {
